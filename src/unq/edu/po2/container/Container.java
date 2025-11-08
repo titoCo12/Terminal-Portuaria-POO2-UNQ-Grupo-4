1e8a4934cacc318ca82d0be4db41c84e4674cba7
Leandro Bogarín <73348664+BogarLean@users.noreply.github.com>
2025-11-08 18:08:38 -0300

    .

--- src/unq/edu/po2/container/Container.java
+++ src/unq/edu/po2/container/Container.java
@@ -1,5 +1,10 @@
 package unq.edu.po2.container;
 
 public class Container {
+	String identificador;
+	int anchoMetros;
+	int largoMetros;
+	int alturaMetros;
+	int pesoKilos; 
 
 }
--- test/unq/edu/po2/containerTest/ContainerTest.java
+++ test/unq/edu/po2/containerTest/ContainerTest.java
@@ -1,19 +1,33 @@
 package unq.edu.po2.containerTest;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import unq.edu.po2.cliente.Cliente;
+import unq.edu.po2.container.ContainerDry;
+import unq.edu.po2.container.ContenidoCarga;
+
 class ContainerTest {
+	
+	Cliente clienteBruno = new Cliente("Bruno Diaz");
 
 	@BeforeEach
-	void setUp() throws Exception {
-	}
-
+	public void setup() {
+		
+    }
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    public void crearContainerDryConCliente() {
+		
+        ContainerDry dry = new ContainerDry("ABCD1234567", 2, 2, 2, 100, clienteBruno);
+
+        assertEquals("ABCD1234567", dry.getIdentificador());
+        assertEquals("Bruno Diaz", dry.getCliente().getNombre());
+    }
+
+
 
 }
