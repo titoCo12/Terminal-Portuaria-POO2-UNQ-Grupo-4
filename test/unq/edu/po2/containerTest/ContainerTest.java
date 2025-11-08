61547d24bce50eac5ae3609b32dc0599113cf039
Leandro Bogarín <73348664+BogarLean@users.noreply.github.com>
2025-11-07 23:07:05 -0300

    t

---
+++ test/unq/edu/po2/containerTest/ContainerTest.java
@@ -0,0 +1,19 @@
+package unq.edu.po2.containerTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ContainerTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
