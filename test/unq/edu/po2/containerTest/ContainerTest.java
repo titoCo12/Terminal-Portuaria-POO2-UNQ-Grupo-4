6915a92ee799736be91a2ab21788c2c3cb0c24dc
Leandro Bogarín <73348664+BogarLean@users.noreply.github.com>
2025-11-08 23:41:01 -0300

    mock

--- test/unq/edu/po2/containerTest/ContainerTest.java
+++ test/unq/edu/po2/containerTest/ContainerTest.java
@@ -11,10 +11,19 @@ import unq.edu.po2.container.*;
 
 
 class ContainerTest {
-	
+	/*
 	Cliente clienteBruno = new Cliente("Bruno Diaz");
 	Producto arroz = new Producto("Arroz", 40);
 	Producto fideos = new Producto("Fideos", 50);
+	*/
+	Cliente cliente = mock(Cliente.class);
+	Container container = mock(Container.class);
+	ContainerDry dry = mock(ContainerDry.class);
+	ContainerTanque tanque = mock(ContainerTanque.class);
+	ContainerReefer reefer = mock(ContainerReefer.class);
+	ContenidoCarga contenido = mock(ContenidoCarga.class);
+	Producto producto = mock(Producto.class);
+	Contenedor contenedor = mock(Contenedor.class);
 	
 	@BeforeEach
 	public void setUp() {
