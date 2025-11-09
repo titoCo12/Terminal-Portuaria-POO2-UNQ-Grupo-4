8efbd0cbe357f661bf6be9e4fbad13eec32df20a
Leandro Bogarín <73348664+BogarLean@users.noreply.github.com>
2025-11-09 18:32:10 -0300

    Test de servicio

---
+++ test/unq/edu/po2/servicio/ServicioTest.java
@@ -0,0 +1,74 @@
+package unq.edu.po2.servicio;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import unq.edu.po2.container.*;
+
+class ServicioTest {
+	Lavado lavado;
+	Electricidad electricidad;
+	Pesado pesado;
+	AlmacenamientoExcedente excedente;
+	ContainerReefer reefer;
+	//Orden orden;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		lavado = mock(Lavado.class);
+		electricidad = mock(Electricidad.class);
+		pesado = mock(Pesado.class);
+		excedente = mock(AlmacenamientoExcedente.class);
+		reefer = mock(ContainerReefer.class);
+		//orden = mock(Orden.class);
+	}
+
+	@Test
+	void montoFinalDeLavado() {
+		
+		when(lavado.getMontoFinal(reefer)).thenReturn(600.0);
+		
+		double monto = lavado.getMontoFinal(reefer);
+		
+		verify(lavado).getMontoFinal(reefer);
+		
+		assertEquals(600.0, monto);
+	}
+	
+	@Test
+	void montoFinalDeElectricidad() {
+		when(electricidad.getMontoFinal(reefer)).thenReturn(500.0);
+		
+		double monto = electricidad.getMontoFinal(reefer);
+		
+		verify(electricidad).getMontoFinal(reefer);
+		
+		assertEquals(500.0, monto);
+	}
+	
+	@Test
+	void montoFinalDePesado() {
+		when(pesado.getMontoFinal(reefer)).thenReturn(200.0);
+		
+		double monto = pesado.getMontoFinal(reefer);
+		
+		verify(pesado).getMontoFinal(reefer);
+		
+		assertEquals(200.0, monto);
+	}
+	
+	@Test
+	void montoFinalDeAlmacenamientoExcedente() {
+		when(excedente.getMontoFinal(reefer)).thenReturn(1500.0);
+		
+		double monto = excedente.getMontoFinal(reefer);
+		
+		verify(excedente).getMontoFinal(reefer);
+		
+		assertEquals(1500.0, monto);
+	}
+
+}
