9363ed8378839200b2f32ee01a834a818a225928
titoCo12 <joaquinfuentes170@gmail.com>
2025-11-10 17:40:06 -0300

    retoques reporte muelle

--- src/unq/edu/po2/terminales4/reporte/ReporteDeMuelle.java
+++ src/unq/edu/po2/terminales4/reporte/ReporteDeMuelle.java
@@ -1,7 +1,6 @@
 package unq.edu.po2.terminales4.reporte;
 
 import java.util.*;
-
 import unq.edu.po2.terminales4.buque.*;
 import unq.edu.po2.terminales4.orden.*;
 import unq.edu.po2.terminales4.terminal.*;
@@ -11,15 +10,19 @@ public class ReporteDeMuelle extends Reporte{
 	public void visitBuque(Buque buque, Terminal term) {
 		List<Orden> ordenes = buque.getOrdenesQueCorrespondenA(term);
 		String fecha = ordenes.getFirst().getFechaLlegada().toString();
-		
+		//nombre del buque
 		this.escribir(buque.getNombre() + ": \n");
+		//puerto de referencia
 		this.escribir("puerto: " + term.getPuerto().getNombre() + "\n");
+		//fecha de llegada al puerto
 		this.escribir("llegada: ");
 		this.escribir(fecha + " \n");
+		//fecha de salida del puerto
 		//sale el mismo dia que llega
 		this.escribir("salida: ");
 		this.escribir(fecha + " \n");
-		this.escribir("buques operados: ");
+		//cantidad de containers operados
+		this.escribir("containers operados: ");
 		this.escribir(Integer.toString(ordenes.size()));
 	}
 	
