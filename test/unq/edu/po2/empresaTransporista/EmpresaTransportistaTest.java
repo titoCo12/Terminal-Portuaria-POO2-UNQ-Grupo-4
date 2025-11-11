1d9f9bd84349337fe69968dd7b01ff98d5308ec2
Leandro Bogarín <73348664+BogarLean@users.noreply.github.com>
2025-11-11 17:12:18 -0300

    Creado el package y el test

---
+++ test/unq/edu/po2/empresaTransporista/EmpresaTransportistaTest.java
@@ -0,0 +1,19 @@
+package unq.edu.po2.empresaTransporista;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class EmpresaTransportistaTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
