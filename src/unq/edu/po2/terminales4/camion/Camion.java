9e59e972a83a5046d9e15d8dcc44e2938349c457
Leandro Bogarín <73348664+BogarLean@users.noreply.github.com>
2025-11-11 15:42:36 -0300

    Sacando carga del camion

--- src/unq/edu/po2/terminales4/camion/Camion.java
+++ src/unq/edu/po2/terminales4/camion/Camion.java
@@ -1,35 +1,19 @@
 package unq.edu.po2.terminales4.camion;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import unq.edu.po2.chofer.Chofer;
-import unq.edu.po2.container.*;
-import unq.edu.po2.terminales4.orden.Orden;
-import unq.edu.po2.terminales4.terminal.Terminal;
 
 public class Camion {
 	
 	String matricula;
 	Chofer chofer;
-	List<Container> carga;
 	
 	public Camion(String matricula, Chofer chofer) {
 		this.matricula = matricula;
 		this.chofer = chofer;
-		this.carga = new ArrayList<>();
 	}
 	
 	public Chofer getChofer() {
 		return chofer;
-	}
-	
-	public void entregarCarga(Terminal terminal, Orden orden, Camion camion) {
-		//Le agrego a la terminal el container con la orden y saco la carga con esa orden de la lista
-	}
-	
-	public void recibirCarga(Container container) {
-		carga.add(container);
 	}
 
 	public String getPatente() {
