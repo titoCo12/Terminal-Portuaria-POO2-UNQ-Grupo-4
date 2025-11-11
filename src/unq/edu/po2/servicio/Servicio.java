13c7d83e10b05c703fd04230e586899c69578ccf
Leandro Bogarín <73348664+BogarLean@users.noreply.github.com>
2025-11-10 23:56:46 -0300

    prueba

--- src/unq/edu/po2/servicio/Servicio.java
+++ src/unq/edu/po2/servicio/Servicio.java
@@ -7,6 +7,6 @@ public interface Servicio {
 	public double getMontoFinal(Container container);
 	
 	public String getNombre();
-	// si hago return this.getClass().getSimpleName(); deja de ser interface
+	// si hago return this.getClass().getSimpleName(); deja de ser interface.
 
 }
