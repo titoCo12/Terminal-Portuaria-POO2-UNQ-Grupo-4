6ee1f218590ead0e2d7928c845100ebf80e1e159
Leandro Bogarín <73348664+BogarLean@users.noreply.github.com>
2025-11-11 20:52:40 -0300

    cambio de ubicación en naviera

--- src/unq/edu/po2/terminales4/viajes/Naviera.java
+++ src/unq/edu/po2/terminales4/viajes/Naviera.java
@@ -1,5 +1,41 @@
 package unq.edu.po2.terminales4.viajes;
 
-public class Naviera {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import unq.edu.po2.terminales4.circuito.Circuito;
+import unq.edu.po2.terminales4.posicion.Puerto;
+
+public class Naviera {
+	String nombre;
+	List<Circuito> circuitos;
+	
+	public Naviera (String nombre) {
+		this.nombre = nombre;
+		this.circuitos = new ArrayList<>();
+	}
+	
+	public void agregarCircuito(Circuito circuito) {
+		circuitos.add(circuito);
+	}
+	/*
+	public int mejorTiempoLlegadaA(Puerto puertoA, Puerto puertoB) {
+		
+		List<Circuito> conectados = circuitosConectados(puertoA, puertoB);
+		return tiempoMinimo(conectados, puertoA, puertoB);
+	}
+	
+	private List<Circuito> circuitosConectados(Puerto puertoA, Puerto puertoB) {
+		
+		return circuitos.stream().filter(c -> c.contieneRuta(puertoA, puertoB))
+				.collect(Collectors.toList());
+	}
+	
+	private int tiempoMinimo(List<Circuito> conectados, Puerto pOrigen, Puerto pDestino) {
+		
+		return conectados.stream().mapToInt(c -> c.diasDesdeHasta(pOrigen, pDestino))
+				.min().getAsInt();
+	}
+	*/
 }
---
+++ test/unq/edu/po2/naviera/NavieraTest.java
@@ -0,0 +1,20 @@
+package unq.edu.po2.naviera;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class NavieraTest {
+	
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
