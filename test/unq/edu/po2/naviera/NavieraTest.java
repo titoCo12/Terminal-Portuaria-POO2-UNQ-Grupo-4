899ec6e1a045b3fd00e39d128f03f7972012ca4a
Leandro Bogarín <73348664+BogarLean@users.noreply.github.com>
2025-11-12 19:49:44 -0300

    test naviera

--- test/unq/edu/po2/naviera/NavieraTest.java
+++ test/unq/edu/po2/naviera/NavieraTest.java
@@ -1,18 +1,28 @@
 package unq.edu.po2.naviera;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import unq.edu.po2.terminales4.circuito.Circuito;
 import unq.edu.po2.terminales4.viajes.Naviera;
 
 class NavieraTest {
 	Naviera jimmy;
+	Circuito circuito1;
+	Circuito circuito2;
+	List<Circuito> circuitos;
 
 	@BeforeEach
 	void setUp() throws Exception {
-		jimmy = new Naviera("Jimmy");
+		circuito1 = mock(Circuito.class);
+		circuito2 = mock(Circuito.class);
+		circuitos = Arrays.asList(circuito1, circuito2);
+		jimmy = new Naviera("Jimmy", circuitos);
 	}
 
 	@Test
