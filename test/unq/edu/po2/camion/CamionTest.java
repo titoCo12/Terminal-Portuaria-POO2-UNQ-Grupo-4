dfee926b3c440307b21dc9bbdb4503f0b35c4ce7
Leandro Bogarín <73348664+BogarLean@users.noreply.github.com>
2025-11-12 00:05:53 -0300

    Test de camion

--- test/unq/edu/po2/camion/CamionTest.java
+++ test/unq/edu/po2/camion/CamionTest.java
@@ -8,37 +8,35 @@ import org.junit.jupiter.api.Test;
 
 import unq.edu.po2.chofer.Chofer;
 import unq.edu.po2.terminales4.camion.Camion;
+import unq.edu.po2.terminales4.orden.Orden;
+import unq.edu.po2.terminales4.terminal.Terminal;
 
 class CamionTest {
 	Chofer chofer;
 	Camion camion;
+	Orden orden;
+	Terminal terminal;
 
 	@BeforeEach
 	void setUp() throws Exception {
 		
-		chofer = mock(Chofer.class);
-		camion = mock(Camion.class);
+		chofer = new Chofer("Manuel Santos", "35.201.151");
+		camion = new Camion("ABC123", chofer, orden, terminal);
+		orden = mock(Orden.class);
+		terminal = mock(Terminal.class);
 		
 	}
 
 	@Test
 	void patenteCamion() {
-		when(camion.getPatente()).thenReturn("ABC123");
-		
 		String patente = camion.getPatente();
-		
-		verify(camion).getPatente();
 		
 		assertEquals("ABC123", patente);
 	}
 	
 	@Test 
 	void choferEnCamion(){
-		when(camion.getChofer()).thenReturn(chofer);
-		
 		Chofer choferAsignado = camion.getChofer();
-		
-		verify(camion).getChofer();
 		
 		assertEquals(chofer, choferAsignado);
 	}
