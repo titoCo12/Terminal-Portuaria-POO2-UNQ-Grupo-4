3a6c8e2ee83cffe3d36098142807302404ec6dca
Leandro Bogarín <73348664+BogarLean@users.noreply.github.com>
2025-11-12 02:31:05 -0300

    Falta el ultimo test

--- test/unq/edu/po2/servicio/ServicioTest.java
+++ test/unq/edu/po2/servicio/ServicioTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -21,46 +22,43 @@ class ServicioTest {
 
 	@BeforeEach
 	void setUp() throws Exception {
-		lavado = new Lavado(100, 200);
-		electricidad = new Electricidad(300);
-		pesado = new Pesado(400);
-		excedente = new AlmacenamientoExcedente(1500, 2);
+		lavado = new Lavado(100.0, 200.0);
+		electricidad = new Electricidad(300.0);
+		pesado = new Pesado(400.0);
+		excedente = new AlmacenamientoExcedente(1500.0, 2);
 		reefer = mock(ContainerReefer.class);
 		orden = mock(Orden.class);
 	}
 
 	@Test
 	void montoFinalDeLavado() {
-		
-		when(lavado.getMontoFinal(reefer)).thenReturn(600.0);
+		when(reefer.getDimensionesCubicas()).thenReturn(80);
 		
 		double monto = lavado.getMontoFinal(reefer);
 		
-		verify(lavado).getMontoFinal(reefer);
+		assertEquals(200, monto);
+	}
+	
+	@Test
+	void montoFinalDeElectricidad() {
+		/*
+		 * LocalDateTime.of(2022, 12, 18, 12, 00), 
+		 * LocalDateTime.of(2026, 06, 11, 12, 00));
+		 */
+		when(reefer.getConsumoXHora()).thenReturn(100.0);
+		when(reefer.getMomentoConexion()).thenReturn(LocalDateTime.of(2025, 11, 11, 12, 00));
+		when(reefer.getMomentoDesconexion()).thenReturn(LocalDateTime.of(2025, 11, 11, 14, 00));
+		
+		double monto = electricidad.getMontoFinal(reefer);
 		
 		assertEquals(600.0, monto);
 	}
 	
 	@Test
-	void montoFinalDeElectricidad() {
-		when(electricidad.getMontoFinal(reefer)).thenReturn(500.0);
-		
-		double monto = electricidad.getMontoFinal(reefer);
-		
-		verify(electricidad).getMontoFinal(reefer);
-		
-		assertEquals(500.0, monto);
-	}
-	
-	@Test
 	void montoFinalDePesado() {
-		when(pesado.getMontoFinal(reefer)).thenReturn(200.0);
+		double monto = pesado.getMontoFinal(reefer); 
 		
-		double monto = pesado.getMontoFinal(reefer);  
-		 
-		verify(pesado).getMontoFinal(reefer);
-		
-		assertEquals(200.0, monto);
+		assertEquals(400.0, monto);
 	}
 	
 	@Test
