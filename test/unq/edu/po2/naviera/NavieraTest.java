199148ef3f0082d4bbf6d5a4226cab69c603c5d3
Leandro Bogarín <73348664+BogarLean@users.noreply.github.com>
2025-11-12 23:01:59 -0300

    test de naviera

--- test/unq/edu/po2/naviera/NavieraTest.java
+++ test/unq/edu/po2/naviera/NavieraTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import unq.edu.po2.terminales4.circuito.Circuito;
+import unq.edu.po2.terminales4.posicion.Puerto;
 import unq.edu.po2.terminales4.viajes.Naviera;
 
 class NavieraTest {
@@ -16,6 +17,8 @@ class NavieraTest {
 	Circuito circuito1;
 	Circuito circuito2;
 	List<Circuito> circuitos;
+	Puerto origen;
+	Puerto destino;
 
 	@BeforeEach
 	void setUp() throws Exception {
@@ -23,11 +26,24 @@ class NavieraTest {
 		circuito2 = mock(Circuito.class);
 		circuitos = Arrays.asList(circuito1, circuito2);
 		jimmy = new Naviera("Jimmy", circuitos);
+		origen = mock(Puerto.class);
+		destino = mock(Puerto.class);
+		
 	}
 
 	@Test
 	void nombreDeNaviera() {
 		assertEquals("Jimmy", jimmy.getNombre());
+	}
+	
+	@Test
+	void menorTiempoHastaOrigenYDestino() {
+		when(circuito1.diasDesdeHasta(origen, destino)).thenReturn(5);
+		when(circuito2.diasDesdeHasta(origen, destino)).thenReturn(3);
+		
+		int tiempo = jimmy.mejorTiempoHasta(origen, destino);
+		
+		assertEquals(3, tiempo);
 	}
 
 }
